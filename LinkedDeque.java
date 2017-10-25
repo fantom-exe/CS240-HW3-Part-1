5fa7efcf1b2fa4e0e26c16fe1d7096527fbe0749
Ed <edoisa6@gmail.com>
2017-10-25 03:54:48 -0700

    in progress

--- LinkedDeque.java
+++ LinkedDeque.java
@@ -1,55 +1,77 @@
 
 
 /**
- * A class implementing a Deque using linked linked chain
+ * A class implementing a Deque using single linked chain
  */
 
-public final class LinkedDeque<T> implements DequeInterface<T> {
+public final class SingleLinkedDeque<T> implements DequeInterface<T> {
+	// member variables
+	private Node frontNode;
+	private Node backNode;
+	
+	/** Adds a new entry to the front/back of this dequeue.
+	 @param newEntry  An object to be added. */
+	public void addToFront(T newEntry) {
+	
+	}
+	
+	public void addToBack(T newEntry) {
+	
+	}
+	
+	/** Removes and returns the front/back entry of this dequeue.
+	 @return  The object at the front/back of the dequeue.
+	 @throws  EmptyQueueException if the dequeue is empty before the operation. */
+	public T removeFront() {
+	
+	}
+	
+	public T removeBack() {
+	
+	}
+	
+	/** Retrieves the front/back entry of this dequeue.
+	 @return  The object at the front/back of the dequeue.
+	 @throws  EmptyQueueException if the dequeue is empty before the operation. */
+	public T getFront() {
+	
+	}
+	
+	public T getBack() {
+	
+	}
+
+/*  Detects whether this dequeue is empty.
+   @return  True if the queue is empty, or false otherwise. */
+	public boolean isEmpty() {
+	
+	}
+
+/*  Removes all entries from this dequeue. */
+	public void clear() {
+	
+	}
+	
+	private class Node {
+		private T data;
+		private Node next;
 		
-		/** Adds a new entry to the front/back of this dequeue.
-		 @param newEntry  An object to be added. */
-		public void addToFront(T newEntry);
-		public void addToBack(T newEntry);
-		
-		/** Removes and returns the front/back entry of this dequeue.
-		 @return  The object at the front/back of the dequeue.
-		 @throws  EmptyQueueException if the dequeue is empty before the operation. */
-		public T removeFront();
-		public T removeBack();
-		
-		/** Retrieves the front/back entry of this dequeue.
-		 @return  The object at the front/back of the dequeue.
-		 @throws  EmptyQueueException if the dequeue is empty before the operation. */
-		public T getFront();
-		public T getBack();
-   
-   /*  Detects whether this dequeue is empty.
-       @return  True if the queue is empty, or false otherwise. */
-		public boolean isEmpty();
-   
-   /*  Removes all entries from this dequeue. */
-		public void clear();
-		
-		private class Node {
-			private T data;
-			private Node next;
-			
-			public T getData() {
-			
-			}
-			
-			private void setData(T newData) {
-			
-			}
-			
-			public Node getNextNode() {
-			
-			}
-			
-			private void setNextNode(Node newNode) {
-			
-			}
-			
+		public T getData() {
+			return data;
 		}
+		
+		private void setData(T newData) {
+			data = newData;
+		}
+		
+		public Node getNextNode() {
+			return next;
+		}
+		
+		private void setNextNode(Node newNode) {
+			next = newNode;
+		}
+		
+	}
 	
 }
